b5673189144eab2703f3223c10c069893ddb83c6
lidehua <lidehua@meituan.com>
2016-04-22 01:03:49 +0800

    deploy

---
+++ script/deploy-tmp.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+app=pearl
+module=pearl
+version=0.0.1
+jar=$module-$version-SNAPSHOT.jar
+pid=`ps -ef | grep $jar | grep -v grep | awk '{print $2}'`
+
+webroot=/opt/$app
+if [ ! -d $webroot ] ; then
+    sudo mkdir $webroot && sudo chown -R devel:devel $webroot
+fi
+
+cd $webroot
+
+if [ -n "$pid" ] ; then
+    kill $pid
+fi
+
+cp /tmp/$app/target/$jar ./
+
+nohup java -jar $jar > /dev/null 2>&1 &
+echo $! > $module.pid
